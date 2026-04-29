c3fda8b4a16c7057f16ec412cb06a0b2a568df5f
Oleksandr Ivanyshyn <o.ivanyshyn.dev@gmail.com>
2026-04-29 15:33:13 +0300

    feat: add Avatar customization and functionality components

--- src/App.jsx
+++ src/App.jsx
@@ -26,12 +26,34 @@ import {
   ToggleButtonFunctionality,
   ToggleButtonCustomization,
 } from './components/inputs';
+import {
+  AvatarFunctionality,
+  AvatarCustomization,
+  BadgeFunctionality,
+  BadgeCustomization,
+  ChipFunctionality,
+  ChipCustomization,
+  DividerFunctionality,
+  DividerCustomization,
+  ListFunctionality,
+  ListCustomization,
+  MuiIconsFunctionality,
+  MuiIconsCustomization,
+  SvgIconFunctionality,
+  SvgIconCustomization,
+  TableFunctionality,
+  TableCustomization,
+  TooltipFunctionality,
+  TooltipCustomization,
+  TypographyFunctionality,
+  TypographyCustomization,
+} from './components/data_display';
 
 export default function App() {
   return (
     <>
       <h1>Welcome to Mastering MUI: Component Customization</h1>
-      <ToggleButtonCustomization />
+      <AvatarCustomization />
     </>
   );
 }
